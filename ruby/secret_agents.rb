4d122250ef467bf602237839f0c2579997627c7b
bteixeira1 <bteixeira3@icloud.com>
2017-06-25 16:40:00 -0400

    Correct file

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,59 @@
+# encrypt
+# - Loop through the string
+#   - Use .next! method on the letter
+def encrypt(string)
+  for i in 0...string.length
+    string[i] = string[i].next[0]
+  end
+  string
+end
+
+# print encrypt("string")
+
+# decrypt
+# - Assign a variable to alphabet
+# - Loop through the string
+#   - get the index of each letter and subtract 1
+#   - assign string[i] to alphabet.index(index value from line 16)
+# - Return string
+
+def decrypt(string)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  for i in 0...string.length
+    decrypt_index = alphabet.index(string[i]) - 1
+    string[i] = alphabet[decrypt_index]
+  end
+  string
+end
+
+=begin
+
+encrypt("abc") should return "bcd"
+encrypt("zed") should return "afe"
+decrypt("bcd") should return "abc"
+decrypt("afe") should return "zed"
+
+=end
+
+#puts encrypt("abc")
+#puts encrypt("zed")
+#puts decrypt("bcd")
+#puts decrypt("afe")
+
+#decrypt(encrypt("swordfish"))
+
+
+#The encrypt method runs first returning "txpsegjti" and the decrypt method runs returning "swordfish".
+
+puts "Would you like to decrypt or encrypt a password?"
+password_cryption = gets.chomp
+puts "What password would you like to #{password_cryption}?"
+password = gets.chomp
+
+if password_cryption == "encrypt"
+  password = encrypt(password)
+elsif password_cryption == "decrypt"
+  password = decrypt(password)
+end
+
+puts "Your #{password_cryption}ed password is #{password}"
